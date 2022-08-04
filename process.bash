486cf901421f0b244307b3d96693753f78fbc7c0
farook <krimfarook.protonmail.com>
2022-08-04 16:48:14 +0300

    I'we added new script process for my ubuntu

---
+++ process.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This part is for Helm configuration 
+sudo microk8s config >/tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
+sudo chmod go-r /tmp/kubeconfig
+# This part is for color of Vim depends of the time of the day
+# 
+# Assign the filename
+filename=".vimrc"
+# Create variable to check the Time 
+TIME=$(date "+%H")
+HOUR=16
+# Creating variables to change colorsheme in vim 
+morning="darkblue"
+evening="pablo"
+
+# Here I check the time 
+if [ $TIME -lt $HOUR ]; then
+        cd ~
+        sed -i "s/$evening/$morning/" $filename
+else        
+        cd ~ 
+        sed -i "s/$morning/$evening/" $filename
+fi
+                                                        
